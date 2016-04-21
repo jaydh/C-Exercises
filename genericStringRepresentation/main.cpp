e9150ff7f0532b609a4bcc0b179ea9de62ed6882
Jay Howard <jaydh@uw.edu>
2016-04-21 16:57:00 -0700

    Added excercise 3.11.2 description in comments

--- genericStringRepresentation/main.cpp
+++ genericStringRepresentation/main.cpp
@@ -2,6 +2,9 @@
 Jay Howard
 Excercise 3.11.1 String Representation
 Write a gerneric function to_string that takes an argument of an arbitrary type (as const&) and generates a string by piping it to a std::stringstream and returning the result string.
+
+Excercise 3.11.2 String Representation of Tuples
+Write a variadic template funciton that represents an arbitrary number of arguments as a tuple in a string. That is, the function call to_tuple_(x,y,z) returns a string of the form (X,y,z) by printing each element to a string stream. Hint: Use a help function to_tuple_string_aux that is overloaded for different arities.
 */
 
 #include<string>

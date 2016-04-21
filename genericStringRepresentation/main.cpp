dfdaf1bada516c0804322d07b5dc7631a0824cac
Jay Howard <jaydh@uw.edu>
2016-04-21 16:55:20 -0700

    Implemented generic function to_string with tests in main

--- genericStringRepresentation/main.cpp
+++ genericStringRepresentation/main.cpp
@@ -4,6 +4,24 @@ Excercise 3.11.1 String Representation
 Write a gerneric function to_string that takes an argument of an arbitrary type (as const&) and generates a string by piping it to a std::stringstream and returning the result string.
 */
 
+#include<string>
+#include<sstream>
+#include<iostream>
+using namespace std;
+
+template <typename T>
+inline string to_string(const T& from) {
+	stringstream to;
+	to << from;
+	return to.str();
+}
+
 int main() {
+	auto testString = "This is a string";
+	auto testChar = 'c';
+	auto testInt = "42";
+
+	cout << to_string(testString) << endl << to_string(testChar) << endl << to_string(testInt) << endl << to_string("This is an rvalue") << endl;
+	system("PAUSE");
 	return 0;
 }

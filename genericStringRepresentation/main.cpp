894db79fc6ea09512f7ea273103771daeeaced6b
Jay Howard <jaydh@uw.edu>
2016-04-21 16:33:58 -0700

    Initialized main and project files

---
+++ genericStringRepresentation/main.cpp
@@ -0,0 +1,9 @@
+/*
+Jay Howard
+Excercise 3.11.1 String Representation
+Write a gerneric function to_string that takes an argument of an arbitrary type (as const&) and generates a string by piping it to a std::stringstream and returning the result string.
+*/
+
+int main() {
+	return 0;
+}

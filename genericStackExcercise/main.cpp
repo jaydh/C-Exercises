2efe2980d2983c85d6e53946f15bdad119857bd4
Jay Howard <jayhoward300@gmail.com>
2016-04-23 01:49:20 -0700

    Initialized main and project files with description and small empty implementations.

---
+++ genericStackExcercise/main.cpp
@@ -0,0 +1,19 @@
+/*Jay Howard
+Excercise 3.11.3 Generic Stack
+Write a stack implementaiton for a genric value type. The maximal size of the stack is define in the class (hard-wired). Provide the following functions: Constructor, Destructor if necessary, top:show last element, pop: remove last elemtn (without returning), push: insert new element, clear: delete all entries, size: number of elements, full: wheter a stack is full, empty: whether a stack is empty.
+*/
+
+template<typename T, int max_size>
+Class genStack{
+public:
+	const static max = max_size;
+	genStack(){}
+	~genStack() {}
+
+private: 
+
+}
+
+int main() {
+	return 0;
+}

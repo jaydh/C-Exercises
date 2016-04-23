55f45855c1ec78e524caa8ee5afd5991db8c408b
jaydh <jaydh@uw.edu>
2016-04-23 13:26:05 -0700

    Implemented generic stack with vector.

--- genericStackExcercise/main.cpp
+++ genericStackExcercise/main.cpp
@@ -1,19 +1,53 @@
 /*Jay Howard
 Excercise 3.11.3 Generic Stack
-Write a stack implementaiton for a genric value type. The maximal size of the stack is define in the class (hard-wired). Provide the following functions: Constructor, Destructor if necessary, top:show last element, pop: remove last elemtn (without returning), push: insert new element, clear: delete all entries, size: number of elements, full: wheter a stack is full, empty: whether a stack is empty.
+Write a stack implementaiton for a generic value type. The maximal size of the stack is defined in the class (hard-wired). Provide the following functions: Constructor, Destructor if necessary, top:show last element, pop: remove last elemtn (without returning), push: insert new element, clear: delete all entries, size: number of elements, full: wheter a stack is full, empty: whether a stack is empty.
+
+I interpret the problem to mean the stack can contain any value type, but only one. 
 */
 
-template<typename T, int max_size>
-Class genStack{
+#include<assert.h>
+#include<vector>
+#include<string>
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+template<typename T>
+class genStack{
 public:
-	const static max = max_size;
-	genStack(){}
-	~genStack() {}
+	explicit genStack(int maxSize) :the_max_size(maxSize) { }
+	~genStack() { }
+
+	T top() { return the_stack.back(); }
+	
+	T pop() { 
+		auto toPop = the_stack.back();
+		the_stack.pop_back();
+		return toPop;
+	}
+
+	template<typename P>
+	void push(P p) { 
+		assert(the_stack.size() <= the_max_size);
+		//assert(typeid(top()) == typeid(p));
+		the_stack.push_back(p); 
+	}
+	void clear(T t) { the_stack.clear(); }
+	int size() const { return the_stack.size(); }
+	bool full() const { return the_stack.size() == the_max_size; }
+	bool empty() const { return the_stack.empty(); }
 
 private: 
-
-}
+	const int the_max_size;
+	vector<T> the_stack;
+};
 
 int main() {
+	genStack<string> G(255);
+	G.push("this is a push");
+	G.push("keep on pushing!");
+	cout << G.pop() << endl;;
+	system("pause");
 	return 0;
 }

41ea91a2b59e7fdc4b66fb1846ec512d24da0c44
jayhoward <jayhoward@JAYHOWARDED15>
2016-05-01 04:19:01 -0700

    Initialized files for derivative functor/lambda excercises.

---
+++ CH3 Generic Programming/derivativeFunctor/derivativeFunctor/main.cpp
@@ -0,0 +1,7 @@
+/*Jay Howard
+3.11.11 Functor
+Write a functor for 2 cosx + x^2 and compute the first and second derivatives with the function from Section 3.8.1
+
+3.11.12 Lambda
+Compute the same derivatives as in Exercise 3.11.11 but this time with a lambda expression.*/
+

1305c1e64e0d1a7f4a11557ef6b8f703d19e97dc
jayhoward <jayhoward@JAYHOWARDED15>
2016-05-04 12:09:55 -0700

    Added main with excercise description.

---
+++ CH4 Libraries/sortingByMagnitude/main.cpp
@@ -0,0 +1,9 @@
+/*Jay Howard
+Excercise 4.8.1 Sorting by Magnitude
+Create a vector of double and initialize it with the valeus -9.3, -7.4, -3.8, -0.4, 1.3, 3.9, 5.4, 8.2. 
+You can use an initializer list. Sort the values by magnitude. Write a functor and a lambda expression for the comparison. Try both solutions.
+*/
+
+int main() {
+	return 0;
+}

c51f9ce6353d416b48727fb1f5839e397f2b06c7
jayhoward <jayhoward@JAYHOWARDED15>
2016-05-04 14:16:32 -0700

    Added two implementations of magnitude sort, one with a functor and one with a lambda.

--- CH4 Libraries/sortingByMagnitude/main.cpp
+++ CH4 Libraries/sortingByMagnitude/main.cpp
@@ -1,9 +1,33 @@
 /*Jay Howard
 Excercise 4.8.1 Sorting by Magnitude
-Create a vector of double and initialize it with the valeus -9.3, -7.4, -3.8, -0.4, 1.3, 3.9, 5.4, 8.2. 
+Create a vector of double and initialize it with the valeus -9.3, -7.4, -3.8, -0.4, 1.3, 3.9, 5.4, 8.2.
 You can use an initializer list. Sort the values by magnitude. Write a functor and a lambda expression for the comparison. Try both solutions.
 */
 
+#include<vector>
+#include<algorithm>
+#include<iostream>
+
+template <typename T>
+struct magnitudeSort {
+	bool operator()(T i, T j) { return std::abs(i) < std::abs(j); }
+};
+
 int main() {
+	std::vector<double> v1 { -9.3, -7.4, -3.8, -0.4, 1.3, 3.9, 5.4, 8.2};
+	std::vector<double> v2 = v1;
+	magnitudeSort<double> m;
+	std::sort(v1.begin(), v1.end(), m);
+	std::sort(v2.begin(), v2.end(), [](double i, double j) {return std::abs(i) < std::abs(j); });
+
+	for (auto x : v1) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+	for (auto x : v2) {
+		std::cout << x << " ";
+	}
+	std::cin.get();
+
 	return 0;
 }

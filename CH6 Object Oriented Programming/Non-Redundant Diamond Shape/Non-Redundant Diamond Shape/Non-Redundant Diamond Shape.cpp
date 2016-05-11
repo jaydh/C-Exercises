b155ca3adbe2a8a82317bfbf5171665d5800b3d0
jay <jay@JAYHOWARD71CE>
2016-05-11 16:54:19 -0700

    Made mathematician and student class virtual. Still need to iron out member methods.

--- CH6 Object Oriented Programming/Non-Redundant Diamond Shape/Non-Redundant Diamond Shape/Non-Redundant Diamond Shape.cpp
+++ CH6 Object Oriented Programming/Non-Redundant Diamond Shape/Non-Redundant Diamond Shape/Non-Redundant Diamond Shape.cpp
@@ -1,6 +1,6 @@
 /* Jay Howard
 Excercise 6.7.1 Non-redudnat Diamond Shape
-Implement the diamond shape from Section 6.3.2 such that the name is only printed once. Distinguish in derived classes between all_info() and my_infods() and call the two functions appropriately. 
+Implement the diamond shape from Section 6.3.2 such that the name is only printed once. Distinguish in derived classes between all_info() and my_infos() and call the two functions appropriately. 
 */
 
 #include "stdafx.h"
@@ -15,76 +15,48 @@ public:
 
 	void set_name(const string& n) { name = n; }
 	string get_name() const { return name; }
-	virtual void all_info() const { cout << "[person]	My name is " << name << endl; }
+	virtual void my_infos() const { cout << "My name is " << name << endl; }
+	virtual void all_info() const { my_infos(); }
 private:
 	string name;
 };
 
 class student
-	: public person
+	: public virtual person
 {
 public:
 	student(const string& name, const string& passed) : person(name), passed(passed) {}
-	virtual void all_info() const {
-		cout << "[student] My name is " << get_name() << endl;
-		cout << "I passed the following grades: " << passed << endl;
-	}
+	virtual void my_infos() const { cout << "I passed the following grades: " << passed << endl;}
+	
 private:
 	string passed;
 };
 
 class mathematician : 
-	public person{
+	public virtual person{
 public:
-	mathematician(const string& name, const string& proved) : name(name), proved(proved) {}
-	virtual void all_info() const override {
-		person::all_info();
-		cout << "	I proved : " << proved << endl;
-	}
-private:
-	string name;
-	string proved;
-};
+	mathematician(const string& name, const string& proved) : person(name), proved(proved) {}
+	virtual void my_infos() const override { cout << "I proved : " << proved << endl; }
 
-class student_updated
-	: public person
-{
-public:
-	student_updated(const string& name, const string& passed) : person(name), passed(passed) {}
-	virtual void all_info() const {
-		person::all_info();
-		cout << "I passed the following grades: " << passed << endl;
-	}
-private:
-	string passed;
-};
-class mathematician_updated :
-	public person {
-public:
-	mathematician_updated(const string& name, const string& proved) : person(name), proved(proved) {}
-	virtual void all_info() const override {
-		person::all_info();
-		cout << "	I proved : " << proved << endl;
-	}
 private:
 	string proved;
 };
-
 class math_student :
-	public student_updated, public mathematician_updated {
+	public student, public mathematician {
 public:
-	math_student(const string& name, const string& passed, const string& proved) : student_updated(name, passed), mathematician_updated(name, proved){}
-	virtual void all_info() const override {
-		student_updated::all_info();
-		mathematician_updated::all_info();
+	math_student(const string& name, const string& passed, const string& proved) : student(name, passed), mathematician(name, proved){}
+	
+	virtual void my_infos() const override {
+		student::my_infos();
+		mathematician::my_infos();
 	}
-
 };
 
 int main()
 {
 	math_student bob("Robert Robson", "Algebra", "Fermat's Last Theorem");
 	bob.all_info();
+
 	cin.get();
     return 0;
 }

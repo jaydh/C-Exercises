a2034cd39acaa300a5b81ed9dd16ddb715cd3b7a
MicheleBe <miki.belotti@gmail.com>
2017-05-22 21:45:03 +0200

    Update main.cpp
    better pass value by reference

--- CH1 Basics/ageExcercise/ageExcercise/main.cpp
+++ CH1 Basics/ageExcercise/ageExcercise/main.cpp
@@ -3,7 +3,7 @@
 //  Excercises
 //
 //  Created by Jay Howard on 4/14/16.
-//  Copyright © 2016 Jay Howard. All rights reserved.
+//  Copyright Â© 2016 Jay Howard. All rights reserved.
 /*
 From "Discovering Modern C++ An Intensive Course for Scientists, Engineers, and Programmerss"
 Excercise 1.10.1 Age: Write a program that asks input from the keyboard and prints teh result on the screen and writes it to a file. The quesiton is: "What is your age?"
@@ -16,7 +16,7 @@ Excercise 1.10.1 Age: Write a program that asks input from the keyboard and prin
 
 using namespace std;
 
-void stringToOstream(string s, ostream& o) {
+void stringToOstream(const string& s, ostream& o) {
 	if (o.good()) {
 		o << s;
 	}
